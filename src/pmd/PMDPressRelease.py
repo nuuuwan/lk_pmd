2ae5276183966d7f3f287f5f9eadffe2f5df6197
nuuuwan <nuuuwan@gmail.com>
2025-09-26 08:17:26 +0530

    change page url logic

--- src/pmd/PMDPressRelease.py
+++ src/pmd/PMDPressRelease.py
@@ -6,28 +6,28 @@ from utils import File, Hash, Log, Parallel
 from scraper import AbstractDoc
 from utils_future import WWW
 
-log = Log('PMDPressRelease')
+log = Log("PMDPressRelease")
 
 
 @dataclass
 class PMDPressRelease(AbstractDoc):
-    URL_BASE = 'https://pmd.gov.lk'
+    URL_BASE = "https://pmd.gov.lk"
 
     LANG_TO_URL_BASE_LANG = {
-        'en': f'{URL_BASE}/news-media',
-        'si': f'{URL_BASE}/si/පුවත්',
-        'ta': f'{URL_BASE}/ta/செய்தி-ஊடக-அறிக்கை',
+        "en": f"{URL_BASE}/news-media",
+        "si": f"{URL_BASE}/si/පුවත්",
+        "ta": f"{URL_BASE}/ta/செய்தி-ஊடக-அறிக்கை",
     }
 
     LANG_TO_NEXT_TEXT = {
-        'en': 'Next Page »',
-        'si': 'ඉදිරි පිටුවට »',
-        'ta': 'அடுத்த பக்கம் »',
+        "en": "Next Page »",
+        "si": "ඉදිරි පිටුවට »",
+        "ta": "அடுத்த பக்கம் »",
     }
 
     @classmethod
     def get_doc_class_label(cls):
-        return 'lk_pmd_press_releases'
+        return "lk_pmd_press_releases"
 
     @classmethod
     def get_doc_class_description(cls):
@@ -35,37 +35,37 @@ class PMDPressRelease(AbstractDoc):
 
     @classmethod
     def get_doc_class_emoji(cls):
-        return '📢'
+        return "📢"
 
     @classmethod
     def scrape_pmd_article(cls, url: str) -> tuple[str, str]:
         www = WWW(url)
         soup = www.soup
         assert soup
-        div = soup.find('div', class_='post-inner')
-        h2 = div.find('h2')
+        div = soup.find("div", class_="post-inner")
+        h2 = div.find("h2")
         article_title = h2.text.strip()
         article_body_paragraphs = []
-        for p in div.find_all('p'):
+        for p in div.find_all("p"):
             article_body_paragraphs.append(p.text.strip())
         return article_title, article_body_paragraphs
 
     @classmethod
-    def parse_article(cls, div, lang, num_set) -> 'PMDPressRelease':
-        h4 = div.find('h4')
+    def parse_article(cls, div, lang, num_set) -> "PMDPressRelease":
+        h4 = div.find("h4")
         description = h4.text.strip()
-        a = h4.find('a')
-        url = a['href']
-        span_date = div.find('span', class_='timeline-date')
-        m_part, d_part, y_part = [int(x) for x in span_date.text.split('-')]
+        a = h4.find("a")
+        url = a["href"]
+        span_date = div.find("span", class_="timeline-date")
+        m_part, d_part, y_part = [int(x) for x in span_date.text.split("-")]
         assert 1 <= m_part <= 12, m_part
         assert 1 <= d_part <= 31, d_part
         assert 0 <= y_part <= 99, y_part
-        date_str = f'20{y_part:02d}-{m_part:02d}-{d_part:02d}'
+        date_str = f"20{y_part:02d}-{m_part:02d}-{d_part:02d}"
         assert (
-            len(date_str) == 10 and date_str[4] == '-' and date_str[7] == '-'
+            len(date_str) == 10 and date_str[4] == "-" and date_str[7] == "-"
         ), date_str
-        num = f'{lang}-{Hash.md5(description)[:6]}'
+        num = f"{lang}-{Hash.md5(description)[:6]}"
 
         if num in num_set:
             return None
@@ -79,19 +79,19 @@ class PMDPressRelease(AbstractDoc):
         )
 
     @classmethod
-    def get_docs_for_page(
-        cls, lang, i_page: int, num_set: set[str]
-    ) -> list['PMDPressRelease']:
-        log.info(f'{lang=}, {i_page=:,}')
+    def get_docs_for_page(cls, lang, i_page: int, num_set: set[str]):
+        log.info(f"{lang=}, {i_page=:,}")
         url_base_lang = cls.LANG_TO_URL_BASE_LANG[lang]
-        url = f'{url_base_lang}/page/{i_page}/'
+        prefix = f"page/{i_page}/" if i_page > 1 else ""
+        url = f"{url_base_lang}/{prefix}"
         www = WWW(url)
         soup = www.soup
-        assert soup
-        divs = soup.find_all('div', class_='post_row')
+        if not soup:
+            return True, []
+        divs = soup.find_all("div", class_="post_row")
 
         next_text = cls.LANG_TO_NEXT_TEXT[lang]
-        a_next_page = soup.find('a', text=next_text)
+        a_next_page = soup.find("a", text=next_text)
         has_no_next_page = a_next_page is None
 
         doc_list = Parallel.map(
@@ -104,7 +104,7 @@ class PMDPressRelease(AbstractDoc):
         return has_no_next_page, doc_list
 
     @classmethod
-    def gen_docs(cls) -> Generator['PMDPressRelease', None, None]:
+    def gen_docs(cls) -> Generator["PMDPressRelease", None, None]:
         doc_list = cls.list_all()
         num_set = (
             set([doc.num for doc in doc_list]) if doc_list else set()
@@ -114,7 +114,7 @@ class PMDPressRelease(AbstractDoc):
         i_page = 1
         while True:
             no_new_docs = True
-            for lang in ['si', 'en', 'ta']:
+            for lang in ["si", "en", "ta"]:
                 if lang in completed_lang_set:
                     continue
                 has_no_next_page, docs_list = cls.get_docs_for_page(
@@ -139,7 +139,7 @@ class PMDPressRelease(AbstractDoc):
         article_title, article_body_paragraphs = self.scrape_pmd_article(
             self.url_metadata
         )
-        content = '\n\n'.join(
+        content = "\n\n".join(
             [
                 self.description,
                 article_title,
